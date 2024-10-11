99e0483a1413714ca2445bb355f23fc056bb1f33
Hadi Mortada <hadi.mortada@isae.edu.lb>
2024-10-11 14:04:39 +0300

    Integrating reverse array function

--- Array_DS.js
+++ Array_DS.js
@@ -1,4 +1,12 @@
-function reverseArray(a) {
-    return a.reverse();
 
+function reverseArray(array) {
+    for(let i=0,j=array.length-1;i<j;i++,j--){
+        let temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+    }return array;
 }
+console.log(reverseArray([1,2,3]));
+// function reverseArray(array) {
+//     return array.reverse();
+// }
