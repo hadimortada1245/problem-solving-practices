2ac3dc626012f1a668b1d94686118a954c75b905
Hadi Mortada <hadi.mortada@isae.edu.lb>
2024-12-06 14:04:25 +0200

    Providing the optimized version

--- maxCount.js
+++ maxCount.js
@@ -1,18 +1,32 @@
+// var maxCount = function(banned, n, maxSum) {
+    
+//     let count=0;
+//     let i=1;
+    
+//     let current =0;
+
+//     while(i<=n){
+//         while(i<=n &&!banned.includes(i) && i<=maxSum && current+i<=maxSum){
+//             current+=i;
+//             count++;
+//             i++;
+    
+//         }i++;
+//     }return count;
+// };
 var maxCount = function(banned, n, maxSum) {
-    
-    let count=0;
-    let i=1;
-    
-    let current =0;
+    const bannedSet = new Set(banned); 
+    let count = 0;
+    let currentSum = 0;
 
-    while(i<=n){
-        while(i<=n &&!banned.includes(i) && i<=maxSum && current+i<=maxSum){
-            current+=i;
-            count++;
-            i++;
-    
-        }i++;
-    }return count;
+    for (let i = 1; i <= n; i++) {
+        if (bannedSet.has(i)) continue;
+        if (currentSum + i > maxSum) break;
+
+        currentSum += i;
+        count++;
+    }
+
+    return count;
 };
-
 console.log(maxCount([11],7,50));
